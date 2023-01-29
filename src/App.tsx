6e830f8ea3054ab9e0e6de0150fa48fb3bc64db8
GB <voducthang16@gmail.com>
2023-01-29 13:01:41 +0700

    Add Tailwind, Prettierrc, Hero & Header UI
    1:00 PM - 29/01/23

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useEffect, useRef } from 'react';
+import avatar from './assets/img/avatar.jpg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+    const header = useRef<HTMLElement>(null);
+    const home = useRef<HTMLElement>(null);
+    const about = useRef<HTMLElement>(null);
+    const portfolio = useRef<HTMLElement>(null);
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    const handleScrollFixed = () => {
+        document.addEventListener('scroll', () => {
+            header.current?.classList.toggle('header-sticky', window.scrollY > 200);
+        });
+    };
+    useEffect(() => {
+        handleScrollFixed();
+    }, []);
+    const scrollToSection = (elementRef: any) => {
+        window.scrollTo({
+            top: elementRef.current.offsetTop,
+        });
+    };
+    return (
+        <div>
+            <section ref={home} className="relative h-screen">
+                <div className="background">
+                    <header ref={header} className="py-4 text-white">
+                        <nav className="container h-10 flex items-center justify-between">
+                            <a className="font-light text-lg" href="/">
+                                Vo Duc Thang
+                            </a>
+                            <ul className="flex space-x-8 text-xl font-normal">
+                                <li>Home</li>
+                                <li onClick={() => scrollToSection(about)}>About</li>
+                                <li onClick={() => scrollToSection(portfolio)}>Portfolio</li>
+                                <li>Contact</li>
+                            </ul>
+                        </nav>
+                    </header>
+                    <div
+                        className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 
+                    content-[''] flex flex-col items-center text-white"
+                    >
+                        <img className="w-52 h-w-52 rounded-full" src={avatar} alt="Vo Duc Thang" />
+                        <h3 className="mt-6 text-5xl font-bold">Vo Duc Thang</h3>
+                        <h5 className="mt-4 text-2xl font-medium">I'm a Web Developer</h5>
+                    </div>
+                </div>
+            </section>
+            <section ref={about} className="container h-screen">
+                TEST ABOUT
+            </section>
+            <section ref={portfolio} className="container h-screen">
+                TEST PORTFOLIO
+            </section>
+        </div>
+    );
 }
 
-export default App
+export default App;
