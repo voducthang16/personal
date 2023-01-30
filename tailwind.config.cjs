410343bb1426d477122c235e820a2c5db07255d0
GB <voducthang16@gmail.com>
2023-01-30 21:34:37 +0700

    Add About, Portfolio, Contact, Footer Section
    9:35 PM - 30/01/23

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        extend: {
+            colors: {
+                primary: '#4169e1',
+                primary_rgba: 'rgba(64, 104, 224, 0.9)',
+                secondary: '#66b95c',
+            },
+        },
+    },
+    plugins: [],
+};
